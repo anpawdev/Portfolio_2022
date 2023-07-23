0dca93ba759aed2f9a511797c9de24c5526d0d44
AniaPawlik <a.pawlik@grafiduo.pl>
2023-07-23 11:45:53 +0200

    + menu

---
+++ resources/assets/js/menu.js
@@ -0,0 +1,24 @@
+import $ from 'jquery'
+
+$('.navbar-toggle').on('click', function () {
+  $('body').removeClass('menu-is-closed').addClass('menu-is-opened')
+})
+
+$('.close-menu, .click-capture, .menu-list li a').on('click', function () {
+  $('body').removeClass('menu-is-opened').addClass('menu-is-closed')
+  $('.menu-list ul').slideUp(300)
+})
+
+$('.menu-list li a').on('click', function () {
+  $('.menu-list li').removeClass('active')
+  $(this).closest('li').addClass('active')
+
+})
+
+$('.col-resume').on('mouseover', function () {
+  $('.section-bg.mask').addClass('hide')
+})
+
+$('.col-resume').on('mouseleave', function () {
+  $('.section-bg.mask').removeClass('hide')
+})
